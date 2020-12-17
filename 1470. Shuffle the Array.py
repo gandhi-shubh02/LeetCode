4735f3fccd2eda776875db15ffdc1ea4d4f0b844
Shubham Gandhi <62895254+gandhi-shubh02@users.noreply.github.com>
2020-12-17 22:10:04 +0300

    Update 1470. Shuffle the Array.py

--- 1470. Shuffle the Array.py
+++ 1470. Shuffle the Array.py
@@ -1,19 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        l=[0 for i in range(2*n)]
-        c=0
-        k=0
-        a=nums[:n]
-        b=nums[n:]
-        for i in range(2*n):
-            if i%2==0:
-                l[i]=a[c]
-                c+=1
-            elif i%2!=0:
-                l[i]=b[k]
-                k+=1
-        return l
-
-            
-        
-                
+        ans=[]
+        for i in range(0,n):
+            ans.append(nums[i])
+            ans.append(nums[n+i])
+        return ans
